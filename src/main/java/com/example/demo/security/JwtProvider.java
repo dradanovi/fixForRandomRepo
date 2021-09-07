247402cf20680616c423b464f90c79afc44164cb
unknown <martinloncar1992@gmail.com>
2021-09-07 20:39:46 +0200

    JWT Filer i JWT provider

--- src/main/java/com/example/demo/security/JwtProvider.java
+++ src/main/java/com/example/demo/security/JwtProvider.java
@@ -2,6 +2,8 @@ package com.example.demo.security;
 
 import com.example.demo.exception.SpringRedditException;
 import com.example.demo.model.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
@@ -11,8 +13,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.security.*;
 import java.security.cert.CertificateException;
-
-import static io.jsonwebtoken.Jwts.parser;
 
 @Service
 public class JwtProvider {
@@ -46,16 +46,33 @@ public class JwtProvider {
             throw new SpringRedditException("Exception occured while retrieving public key from keystore");
         }
     }
-    public void validateToken(String jwt) {
-        parser().setSigningKey(getPublicKey()).parseClaimsJws(jwt);
+    public String validateToken(String jwt) {
+        Jws<Claims> jws = Jwts.parserBuilder().setSigningKey(getPublicKey())
+                .requireAudience("string")
+                .build()
+                .parseClaimsJws(jwt);
+
+        return jwt;
     }
         private PublicKey getPublicKey(){
           try {
               return keyStore.getCertificate("springblog").getPublicKey();
           }catch (KeyStoreException e){
               throw new SecurityException("Exception occured while" +
-                      "retrieving publick key from keystore");
+                      "retrieving public key from keystore");
           }
+        }
+
+        public String getUsernameFromJWT(String token){
+            Claims claims =Jwts.parserBuilder()
+                .requireAudience("string")
+                .build()
+                .parseClaimsJws(token)
+                    .getBody();
+
+
+        return claims.getSubject();
+
         }
 
 }
